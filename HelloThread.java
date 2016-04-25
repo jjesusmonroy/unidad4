d61f3e21a99dc5ae47394fb6ba596a2ca8a6c445
jjesusmonroy <ppchuyjjms@gmail.com>
2016-04-25 07:16:18 -0500

    HOLA

---
+++ HelloThread.java
@@ -0,0 +1,11 @@
+public class HelloThread extends Thread{
+
+  public void run(){
+    System.out.println("Hello from a thread!");
+  }
+
+  public static void main(String[] args) {
+    HelloThread h = new HelloThread();
+    h.start();
+  }
+}
